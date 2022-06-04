e22c345c10094b0a8d17635328b65e29a857268e
Gaston Goren <gaston.goren@osanasalud.com>
2022-06-04 18:56:48 -0300

    Initial

---
+++ src/main/java/com/osana/librarybeta/LibraryBetaActivity.kt
@@ -0,0 +1,11 @@
+package com.osana.librarybeta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LibraryBetaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library_beta)
+    }
+}
